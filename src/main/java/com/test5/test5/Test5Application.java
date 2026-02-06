51dc8212df8d321cff8b92c0d70485ccccf8d0ec
abhishekchouuan@gmail.com <abhishekchouuan@gmail.com>
2026-02-06 12:08:17 +0530

    some thing added

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -10,6 +10,7 @@ public class Test5Application {
 		String name="mike";
 		int x=100;
 		boolean isPresent=false;
+                boolean isSub=true;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
