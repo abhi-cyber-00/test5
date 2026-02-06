bbc74065d9b77e69c04aab44430e7924200e52af
abhi-cyber-00 <abhishekchouuan@gmail.com>
2026-02-06 11:53:29 +0530

    Update Test5Application.java

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x=100;
+		boolean isPresent=false;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
